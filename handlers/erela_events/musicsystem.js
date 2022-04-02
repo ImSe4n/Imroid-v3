bc59e0c9c8bd8902ae8cde546c06f02dc47b8889
_Sean <81334381+ImSe4n@users.noreply.github.com>
2022-04-02 14:25:32 -0400

    Update musicsystem.js

--- handlers/erela_events/musicsystem.js
+++ handlers/erela_events/musicsystem.js
@@ -389,7 +389,7 @@ function generateQueueEmbed(client, guildId, leave){
       new MessageEmbed()
         .setColor(es.color)
         .setFooter(client.getFooter(es))
-        .setImage(guild.banner ? guild.bannerURL({size: 4096}) : "https://imgur.com/jLvYdb4.png")
+        .setImage(guild.banner ? guild.bannerURL({size: 4096}) : "https://i.imgur.com/kStBJS8.png")
         .setTitle(`Start Listening to Music, by connecting to a Voice Channel and sending either the **SONG LINK** or **SONG NAME** in this Channel!`)
         .setDescription(`> *I support <:Youtube:840260133686870036> Youtube, <:Spotify:846090652231663647> Spotify, <:soundcloud:825095625884434462> Soundcloud and direct MP3 Links!*`)
     ]
