d9f36ae605cffd40c4c41b18a4cbe782d9dec202
ImSe4n <montrealcanadiens12310@gmail.com>
2022-04-02 14:06:57 -0400

    first commit

---
+++ commands/🔰 Info/github.js
@@ -0,0 +1,46 @@
+const {
+	MessageEmbed, MessageButton, MessageActionRow
+} = require("discord.js")
+const config = require(`${process.cwd()}/botconfig/config.json`)
+var ee = require(`${process.cwd()}/botconfig/embed.json`)
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const { handlemsg } = require(`${process.cwd()}/handlers/functions`)
+module.exports = {
+	name: "ownergithub",
+	category: "🔰 Info",
+	aliases: ["ownergit", "source", "sourcecode", "src"],
+	cooldown: 2,
+	usage: "ownergithub",
+	description: "Shows you the Github and Source Code Information about this Bot",
+	type: "bot",
+	run: async (client, message, args, cmduser, text, prefix) => {
+		let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
+		
+		try {	
+			let button_public_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.invite).setURL("https://discord.com/api/oauth2/authorize?client_id=734513783338434591&permissions=8&scope=bot%20applications.commands")
+			let button_support_dc = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.dc).setURL("https://dsc.gg/imroid-development")
+			let button_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.botlist).setURL(`https://dsc.gg/imroid-development`)
+			const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
+			message.reply({embeds: [new MessageEmbed()
+				.setColor(es.color)
+				.setFooter(client.getFooter(es))
+				.setTimestamp()
+				.setThumbnail("https://cdn.discordapp.com/avatars/442355791412854784/df7b527a701d9a1ab6d73213576fe295.webp?size=1024")
+				.setTitle(client.la[ls].cmds.info.github?.title)
+				.setURL("https://dsc.gg/imroid-development")
+				.addField("🆕 NEW GITHUB", `> There is now an **open Source** Version of this Bot on [\`Sean#1751\`'s Github](https://github.com/ImSe4n)\n> [Link](https://github.com/ImSe4n/Imroid-v3) but please make sure to **give __Credits__** if you use it!\n> Make sure to read the [README](https://github.com/ImSe4n/Imroid-v3#readme) and the [WIKI / FAQ](https://github.com/ImSe4n/Imroid-v3/wiki) carefully before opening an [ISSUE](https://github.com/ImSe4n/Imroid-v3/issues/new/choose)`)
+			.setDescription(client.la[ls].cmds.info.github?.description)],
+components: allbuttons
+			}).catch(error => console.log(error));
+		} catch (e) {
+			console.log(String(e.stack).grey.bgRed)
+			return message.reply({embeds: [new MessageEmbed()
+			  .setColor(es.wrongcolor)
+			  .setFooter(client.getFooter(es))
+			  .setTitle(client.la[ls].common.erroroccur)
+			  .setDescription(eval(client.la[ls]["cmds"]["info"]["color"]["variable2"]))
+			]});
+		}
+	}
+}
+
---
+++ slashCommands/Info/developer.js
@@ -0,0 +1,43 @@
+const {
+	MessageEmbed
+} = require("discord.js")
+const config = require(`${process.cwd()}/botconfig/config.json`)
+var ee = require(`${process.cwd()}/botconfig/embed.json`)
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const { MessageButton, MessageActionRow } = require('discord.js')
+const { handlemsg } = require(`${process.cwd()}/handlers/functions`)
+module.exports = {
+	name: "developer",
+	description: "Shows Information about the Developer",
+	run: async (client, interaction, cmduser, es, ls, prefix, player, message) => {
+		//things u can directly access in an interaction!
+		const { member, channelId, guildId, applicationId, commandName, deferred, replied, ephemeral, options, id, createdTimestamp } = interaction; 
+		const { guild } = member;
+		
+		try {	
+			let button_public_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.invite).setURL("https://discord.com/api/oauth2/authorize?client_id=734513783338434591&permissions=8&scope=bot%20applications.commands")
+			let button_support_dc = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.dc).setURL("https://dsc.gg/imroid-development")
+			let button_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.botlist).setURL(`https://dsc.gg/imroid-development`)
+			const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
+			interaction?.reply({embeds: [new MessageEmbed()
+				.setColor(es.color)
+				.setFooter(client.getFooter(es))
+				.setTimestamp()
+				.setThumbnail("https://cdn.discordapp.com/avatars/442355791412854784/df7b527a701d9a1ab6d73213576fe295.webp?size=1024")
+				.setTitle(client.la[ls].cmds.info.developer.title)
+				.setURL("https://dsc.gg/imroid-development")
+				.setDescription(client.la[ls].cmds.info.developer.description)],
+components: allbuttons
+			}).catch(error => console.log(error));
+		} catch (e) {
+			console.log(String(e.stack).grey.bgRed)
+			return message.reply({embeds: [new MessageEmbed()
+			  .setColor(es.wrongcolor)
+			  .setFooter(client.getFooter(es))
+			  .setTitle(client.la[ls].common.erroroccur)
+			  .setDescription(eval(client.la[ls]["cmds"]["info"]["color"]["variable2"]))
+			]});
+		}
+	}
+}
+
