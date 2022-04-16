cc1aa9ccf34ef88c96552550b1f62328844557f9
_Sean <81334381+ImSe4n@users.noreply.github.com>
2022-04-16 14:44:17 -0400

    fix font (Layout for lb later)

--- commands/👑 Owner/stopbot.js
+++ commands/👑 Owner/stopbot.js
@@ -16,54 +16,16 @@ module.exports = {
   usage: "stopbot",
   type: "bot",
   description: "Stops the Bot, to set it OFFLINE",
-  run: async (client, message, args, cmduser, text, prefix) => {
-    
-    let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
-    if (!config.ownerIDS.some(r => r.includes(message.author.id)))
-      return message.channel.send({embeds: [new MessageEmbed()
-        .setColor(es.wrongcolor).setFooter(client.getFooter(es))
-        .setTitle(eval(client.la[ls]["cmds"]["owner"]["stopbot"]["variable1"]))
-        .setDescription(eval(client.la[ls]["cmds"]["owner"]["stopbot"]["variable2"]))
-      ]});
-    try {
-      let clientapp = client.application ? await client.application.fetch().catch(e=>false) : false;
-      let guild = client.guilds.cache.get("949744394825760778")
-      return message.reply({content : `**<a:nah:960701828675297300> THIS COMMAND IS DISABLED, go to https://dsc.gg/imroid-development and <#840332764603351101> to get it restarted!**\n\n\n> **Path:**
-\`\`\`yml
-${process.cwd()}
-\`\`\`
-> **Server:**
-\`\`\`yml
-${String(Object.values(require(`os`).networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i?.family===`IPv4` && !i?.internal && i?.address || []), [])), [])).split(".")[3]}
-\`\`\`
-> **Command:**
-\`\`\`yml
-pm2 list | grep "${String(String(process.cwd()).split("/")[String(process.cwd()).split("/").length - 1]).toLowerCase()}" --ignore-case
-\`\`\`
-${clientapp ? `
-> **Application Information:**
-\`\`\`yml
-Link: https://discord.com/developers/applications/${client.user.id}
-Name: ${clientapp.name} 
-${clientapp.owner.discriminator ? "Owner: " + clientapp.owner.tag : "Team: " + clientapp.owner.name + "\n |-> Members: " + clientapp.owner.members.map(uid=>`${uid.user.tag}`).join(", ")  + "\n |-> Team-Owner: " + `${guild.members.cache.get(clientapp.owner.ownerId) && guild.members.cache.get(clientapp.owner.ownerId).user ? guild.members.cache.get(clientapp.owner.ownerId).user.tag : clientapp.owner.ownerId }`} 
-Icon: ${clientapp.iconURL()}
-Bot-Public: ${clientapp.botPublic ? "✅": "❌"} (Invite able)
-\`\`\`
-> **About me:**
-\`\`\`yml
-${clientapp.description ? clientapp.description : "❌ NO DESCRIPTION YET!"}
-\`\`\``
-      : ""}
-      `});
+  run: async (client, message, args, cmduser, text, prefix) => 
 
-      require("child_process").exec(`pm2 stop index.js imroid_bot_${process.cwd().split(require("path").sep).pop()}`, (error, stdout, stderr) => {
+      require("child_process").exec(`pm2 stop index.js ${process.cwd().split(require("path").sep).pop()}`, (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
           message.reply({content : eval(client.la[ls]["cmds"]["owner"]["stopbot"]["variable4"])})
           return;
         }
-      });
-    } catch (e) {
+      }),
+    catch (e) {
       console.log(String(e.stack).dim.bgRed)
       return message.channel.send({embeds : [new MessageEmbed()
         .setColor(es.wrongcolor).setFooter(client.getFooter(es))
@@ -71,5 +33,5 @@ ${clientapp.description ? clientapp.description : "❌ NO DESCRIPTION YET!"}
         .setDescription(eval(client.la[ls]["cmds"]["owner"]["stopbot"]["variable5"]))
       ]});
     }
-  },
-};
+
+  }
